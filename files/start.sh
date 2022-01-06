574cdac450469fbbad63205a32b53ff0de9f1b51
Arpad Kunszt <arpad.kunszt@gmail.com>
2022-01-06 20:22:05 +0100

    start.sh: fix typo

--- files/start.sh
+++ files/start.sh
@@ -6,4 +6,4 @@ export LC_ALL="en_US.UTF-8"
 : ${WORKERS:-8}
 
 . /usr/share/GNUstep/Makefiles/GNUstep.sh
-/usr/sbin/sogod -WOWorkersCount '$WORKERS' -WOLogFile - -WONoDetach YES
+/usr/sbin/sogod -WOWorkersCount "$WORKERS" -WOLogFile - -WONoDetach YES
